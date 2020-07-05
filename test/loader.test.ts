06dd2ab7ea5e8ed7acd37535c713ddce5fd555f0
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-07-05 15:20:09 -0500

    Fix loader test

--- test/loader.test.ts
+++ test/loader.test.ts
@@ -2,13 +2,10 @@ import compile from "./webpack-compile";
 
 test("Inserts name and outputs JavaScript", async () => {
     const stats = await compile("example.txt");
-    /*
+
     const output = stats.toJson();
     const { modules } = output;
 
     expect(modules).toBeDefined();
-    expect(modules && modules[0].source).toBe(`export default "Hey Alice!"`);*/
-    expect(stats).toBeTruthy();
+    expect(modules && modules[0].source).toBe(`export default "Hey Alice!"`);
 });
-
-afterAll(() => console.log("done?"));
